4f7cbd663074270ae1ca97b9e9d1a8bfe7b07b80
Dylan Dove <ddove@ddove-retina.vidmark.local>
2016-01-06 14:55:56 -0500

    Finished testing quite and remove query feature

--- test/basic.js
+++ test/basic.js
@@ -5,53 +5,59 @@ var index = require('../index.js')
 /* BEGIN parse.js tests */
 
 test('parseResource playlist test 1', function (t) {
-	var tagLine = "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=240000";
-	var resourceLine = "prog_index.m3u8";
-	var manifestUri = "http://example.com/hls_manifest.m3u8";
-	var resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+	var 
+	  tagLine = "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=240000",
+	  resourceLine = "prog_index.m3u8",
+	  manifestUri = "http://example.com/hls_manifest.m3u8",
+	  resource = parse.parseResource(tagLine, resourceLine, manifestUri);
 
-    t.plan(2);
+	t.plan(2);
     t.equal(resource.type, 'playlist');
-
     t.equal(resource.line, manifestUri + '/' + resourceLine);
-
 });
 
 test('parseResource playlist test 2', function (t) {
-	var tagLine = "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=440000";
-	var resourceLine = "prog_index2.m3u8";
-	var manifestUri = "http://example.com/hls_manifest.m3u8";
-	var resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+	var 
+	  tagLine = "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=440000",
+	  resourceLine = "prog_index2.m3u8",
+	  manifestUri = "http://example.com/hls_manifest.m3u8",
+	  resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+
     t.plan(2);
     t.equal(resource.type, 'playlist');
     t.equal(resource.line, manifestUri + '/' + resourceLine);
 });
 
 test('parseResource segment test 1', function (t) {
-	var tagLine = "#EXTINF:10, no desc";
-	var resourceLine = "fileSequence0.ts";
-	var manifestUri = "http://example.com/hls_manifest.m3u8";
-	var resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+	var 
+	  tagLine = "#EXTINF:10, no desc",
+	  resourceLine = "fileSequence0.ts",
+	  manifestUri = "http://example.com/hls_manifest.m3u8",
+	  resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+    
     t.plan(2);
     t.equal(resource.type, 'segment');
     t.equal(resource.line, manifestUri + '/' + resourceLine);
 });
 
 test('parseResource segment test 2', function (t) {
-	var tagLine = "#EXTINF:10, no desc";
-	var resourceLine = "http://example.com/hls_manifest.m3u8/fileSequence0.ts";
-	var manifestUri = "http://example.com/hls_manifest.m3u8";
-	var resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+	var 
+	  tagLine = "#EXTINF:10, no desc",
+	  resourceLine = "http://example.com/hls_manifest.m3u8/fileSequence0.ts",
+	  manifestUri = "http://example.com/hls_manifest.m3u8",
+	  resource = parse.parseResource(tagLine, resourceLine, manifestUri);
+
     t.plan(2);
     t.equal(resource.type, 'segment');
     t.equal(resource.line, "http://example.com/hls_manifest.m3u8/fileSequence0.ts");
 });
 
 test('parseManifest test 1', function (t) {
+	var 
+	  manifestUri = "http://example.com/hls_manifest.m3u8",
+	  manifestData = "#EXTM3U\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:10, no desc\nfileSequence0.ts\n#EXTINF:10, no desc\nfileSequence1.ts\n#EXT-X-ENDLIST",
+	  manifestLines = parse.parseManifest(manifestUri, manifestData);
 
-	var manifestUri = "http://example.com/hls_manifest.m3u8";
-	var manifestData = "#EXTM3U\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:10, no desc\nfileSequence0.ts\n#EXTINF:10, no desc\nfileSequence1.ts\n#EXT-X-ENDLIST";
-	var manifestLines = parse.parseManifest(manifestUri, manifestData);
 	var expected = [ { type: 'tag', line: '#EXTM3U' },
   				{ type: 'tag', line: '#EXT-X-TARGETDURATION:10' },
   				{ type: 'tag', line: '#EXT-X-MEDIA-SEQUENCE:0' },
@@ -60,9 +66,9 @@ test('parseManifest test 1', function (t) {
   				{ type: 'tag', line: '#EXTINF:10, no desc' },
   				{ type: 'segment', line: 'http://example.com/fileSequence1.ts' },
   				{ type: 'tag', line: '#EXT-X-ENDLIST' } ];
+    
     t.plan(1);
     t.deepEqual(manifestLines, expected);
-
 });
 
 /* END parse.js tests */
@@ -70,25 +76,28 @@ test('parseManifest test 1', function (t) {
 
 /* BEGIN index.js tests */
 
-test('getCWDName test 1', function (t) {
-	var line = "http://example.com/hls_manifest.m3u8/fileSequence0.ts";
-	var uri = "http://example.com/hls_manifest.m3u8";
+test('getCWDName test 1', function (t) {	
+	var 
+	  line = "http://example.com/hls_manifest.m3u8/fileSequence0.ts",
+	  uri = "http://example.com/hls_manifest.m3u8";
 
 	t.plan(1);
 	t.equal(index.getCWDName(uri, line), 'hls_manifest.m3u8');
 });
 
-//this test also requires parseManifest to be working correctly
-test('createManifestText test 1', function (t) {
-	
-	var uri = "http://example.com/hls_manifest.m3u8";
-	var manifestData = "#EXTM3U\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:10, no desc\nfileSequence0.ts\n#EXTINF:10, no desc\nfileSequence1.ts\n#EXT-X-ENDLIST";
-	var manifestLines = parse.parseManifest(uri, manifestData);
 
-	expected = "#EXTM3U\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:10, no desc\nfileSequence0.ts\n#EXTINF:10, no desc\nfileSequence1.ts\n#EXT-X-ENDLIST"
+/* this test also requires parseManifest to be working correctly */
+test('createManifestText test 1', function (t) {
+	var 
+	  uri = "http://example.com/hls_manifest.m3u8",
+	  manifestData = "#EXTM3U\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:10, no desc\nfileSequence0.ts\n#EXTINF:10, no desc\nfileSequence1.ts\n#EXT-X-ENDLIST",
+	  manifestLines = parse.parseManifest(uri, manifestData),
+	  expected = "#EXTM3U\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:10, no desc\nfileSequence0.ts\n#EXTINF:10, no desc\nfileSequence1.ts\n#EXT-X-ENDLIST";
+	
 	t.plan(1);
 	t.equal(index.createManifestText(manifestLines, uri),expected);
 });
+
 /* END index.js tests */
 
 
